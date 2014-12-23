f699ead38a3473acbdaf3be6c85c4e59c15cc221
thaina <thaina@ThainaNotebook>
2014-12-23 00:59:30 -0200

    Classe Architecture - Adicionado método para adicionar um pointcut

--- src/main/java/arquitetura/representation/Architecture.java
+++ src/main/java/arquitetura/representation/Architecture.java
@@ -624,6 +624,18 @@ public class Architecture extends Variable {
         }
         return false;
     }
+    
+    //Inicio - Thaina 12/14 - Aspectos
+        public boolean addPointcut(Relationship relationship) {
+            if (relationshipHolder.addRelationship(relationship)) {
+                LOGGER.info("Relacionamento: " + relationship.getType() + " adicionado na arquitetura.(" + UtilResources.detailLogRelationship(relationship) + ")");
+                return true;
+            } else {
+                LOGGER.info("TENTOU adicionar Relacionamento: " + relationship.getType() + " na arquitetura porém não consegiu");
+                return false;
+            }
+    }
+    //Fim - Thaina 12/14 - Aspectos
 
     public Package findPackageOfClass(Class targetClass) {
         String packageName = UtilResources.extractPackageName(targetClass.getNamespace());
